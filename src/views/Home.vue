a68989176ffb4f6450ad6feb629525b4be7963ca
MaceiraDev <leo.maceira.santos@hotmail.com>
2023-10-10 13:17:45 -0300

    Primeiro Commit Front

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
